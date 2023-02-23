a5e6b1d2dbaaf5631b483b48194201e0466c1e3d
Azriel Cuellar <2020151541@ub.edu.bz>
2023-02-23 16:37:45 -0600

    Database inserted

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,9 +4,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 // create a new type
@@ -15,20 +21,43 @@ type application struct {
 
 // MAIN PROGRAM BELOW
 func main() {
-
 	//create a flag for speifing the port number when starting the server
 	addr := flag.String("port", ":4000", "HTTP network address")
+	dsn := flag.String(" dsn", os.Getenv("BUSSES_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
-
+	//create an instance of a pool
+	db, err := openDB(*dsn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	//create an instance of the application type
 	app := &application{}
-
+	defer db.Close()
+	log.Println("Database connection pool established")
 	//create our own server (replacing listenandserve()
 	srv := &http.Server{
 		Addr:    *addr,
 		Handler: app.routes(),
 	}
 	log.Printf("Starting server on port %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+// Get a database connection pool
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	//use a context to check if the DB is reachable
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second) //specify timeout
+	defer cancel()                                                          //+Defer attached to a function and executes as the last thing
+	//lets ping the DB
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,16 +6,16 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() http.Handler {
+func (app *application) routes() *httprouter.Router {
 	//Create a multiplexer
-	mux := httprouter.New()
+	router := httprouter.New()
 	//Create a file server
 	fileServer := http.FileServer(http.Dir("./static/"))
-	mux.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer)) //Changed this around to implement the rest capability
-	mux.HandlerFunc(http.MethodGet, "/create", app.Greeting)
-	mux.HandlerFunc(http.MethodGet, "/about", app.About)
-	mux.HandlerFunc(http.MethodGet, "/", app.Home)
-	mux.HandlerFunc(http.MethodPost, "/create", app.MessageCreate)
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer)) //Changed this around to implement the rest capability
+	router.HandlerFunc(http.MethodGet, "/create", app.Greeting)
+	router.HandlerFunc(http.MethodGet, "/about", app.About)
+	router.HandlerFunc(http.MethodGet, "/", app.Home)
+	router.HandlerFunc(http.MethodPost, "/create", app.MessageCreate)
 
-	return mux
+	return router
 }
